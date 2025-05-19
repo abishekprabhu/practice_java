8fcc615df9b8739cf73e1577d36f2f318aa9a5c2
Abishek Prabhu <abiskanna@gmail.com>
2025-05-20 00:26:38 +0530

    spring file update

---
+++ SpringSample/src/test/java/coder/abishekprabhu/springsample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package coder.abishekprabhu.springsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
